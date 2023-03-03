bb1cf2fa65120e690c9db270b6203d45c3f14b64
jgarvel076 <jgarvel076@g.educaand.es>
2023-03-03 09:47:51 +0100

    new commit

--- Tema5/Actividaddes/Actividad5_1.java
+++ Tema5/Actividaddes/Actividad5_1.java
@@ -3,15 +3,15 @@ import java.util.TreeSet;
 public class Actividad5_1 {
     public static void main(String[] args) {
         TreeSet<Persona> Personas = new TreeSet<>(); //ordenar por orden alfabetico y por edad en caso de igual nombre
-        Persona Manolo = new Persona("Juan", 34);
-        Persona Adri = new Persona("Alberto", 21);
-        Persona Pepa = new Persona("Josefa", 64);
-        Persona Ricky = new Persona("Raul", 13);
+        Persona Juan = new Persona("Juan", 34);
+        Persona Alberto = new Persona("Alberto", 21);
+        Persona Josefa = new Persona("Josefa", 64);
+        Persona Raul = new Persona("Raul", 13);
 
-        Personas.add(Manolo);
-        Personas.add(Ricky);
-        Personas.add(Pepa);
-        Personas.add(Adri);
+        Personas.add(Juan);
+        Personas.add(Alberto);
+        Personas.add(Josefa);
+        Personas.add(Raul);
 
         System.out.println(Personas);
 
