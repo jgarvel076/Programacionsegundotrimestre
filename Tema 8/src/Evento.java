abd14002d39e34187fafddab76fbba7035315597
jgarvel076 <jgarvel076@g.educaand.es>
2023-04-27 13:27:22 +0200

    tema8

---
+++ Tema 8/src/Evento.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class Evento implements Serializable {
+    private String nombre;
+    private Calendar fecha;
+    private Integer recaudacion;
+
+}
--- Tema7/src/ejercicio11.java
+++ Tema7/src/ejercicio11.java
@@ -27,20 +27,69 @@ public class ejercicio11 {
         Element contacto = document.createElement("Contacto");
         Lista.appendChild(contacto);
 
-        Element nombrecontacto = document.createElement("nombre");
-        contacto.appendChild(nombrecontacto);
 
         // Creamos un nodo de texto que será el valor del elemento anterior
         Text Pablo = document.createTextNode("Pablo");
         // y lo colgamos del nodo anterior --> <estudiante>Fran</estudiante>
-        contacto1.appendChild(Pablo);
+        contacto.appendChild(Pablo);
 
+        Element direccion = document.createElement("direccion");
+        contacto.appendChild(direccion);
+
+        Text direccionpablo = document.createTextNode("Juan Ramon Jimenez");
+        direccion.appendChild(direccionpablo);
+
+        Element numero = document.createElement("numero");
+        contacto.appendChild(numero);
+
+        Text numeropablo = document.createTextNode("627884659");
+        numero.appendChild(numeropablo);
+
+        Element contacto1 = document.createElement("Contacto");
+        Lista.appendChild(contacto1);
+
+        // Creamos un nodo de texto que será el valor del elemento anterior
+        Text Juan = document.createTextNode("Juan");
+        // y lo colgamos del nodo anterior --> <estudiante>Fran</estudiante>
+        contacto1.appendChild(Juan);
+
+        Element direccion1 = document.createElement("direccion");
+        contacto1.appendChild(direccion1);
+
+        Text direccionjuan = document.createTextNode("Pablo Torres");
+        direccion1.appendChild(direccionjuan);
+
+        Element numero1 = document.createElement("numero");
+        contacto1.appendChild(numero1);
+
+        Text numerojuan = document.createTextNode("602247931");
+        numero1.appendChild(numerojuan);
+
+        Element contacto2 = document.createElement("Contacto");
+        Lista.appendChild(contacto2);
+
+        // Creamos un nodo de texto que será el valor del elemento anterior
+        Text Manuel = document.createTextNode("Manuel");
+        // y lo colgamos del nodo anterior --> <estudiante>Fran</estudiante>
+        contacto2.appendChild(Manuel);
+
+        Element direccion2 = document.createElement("direccion");
+        contacto2.appendChild(direccion2);
+
+        Text direccionmanuel = document.createTextNode("Fernando Torres");
+        direccion2.appendChild(direccionmanuel);
+
+        Element numero2 = document.createElement("numero");
+        contacto2.appendChild(numero2);
+
+        Text numeromanuel = document.createTextNode("697184252");
+        numero2.appendChild(numeromanuel);
 
         // Clases necesarias para finalizar la creación del archivo XML
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         DOMSource source = new DOMSource(document);
-        StreamResult result = new StreamResult(new File("Tema7/src/ejercicio11.java"));
+        StreamResult result = new StreamResult(new File("Tema7/src/ejercicio11.xml"));
 
         // Se realiza la transformación, de Document a Fichero.
         transformer.transform(source, result);
